b3e04569a25768ccbafb3b4d1e0ae1479c4ab92d
Matthew Skomarovsky <matthew.skomarovsky@gmail.com>
2023-07-27 12:08:49 -0400

    fix search bug

--- app/components/NoteList.tsx
+++ app/components/NoteList.tsx
@@ -13,14 +13,14 @@ type NoteListProps = {
 export default function NoteList({ notes, query = "", deleteNote }: NoteListProps) {
   let filteredNotes = query.length === 0 ? 
     notes : 
-    notes.filter(note => note.text.search(query.toLowerCase()) >= 0)
+    notes.filter(note => note.text.toLowerCase().search(query.toLowerCase()) >= 0)
  
   return (
     <div>
       {filteredNotes.map(note => (
         <div key={note.id} className="p-5 mb-5 border border-gray-400 rounded-md bg-white text-black">
           <div>
-            <span className="font-bold">{note.createdAt} &nbsp;</span>
+            <span className="font-bold">{note.createdAt.split("T")[0]} &nbsp;</span>
             <button className="text-sm text-blue-700" onClick={() => deleteNote(note.id)}>delete</button>
           </div>
           <div>
