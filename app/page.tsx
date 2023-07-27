ffdd71a61f6ceb0f42d66bf1e1575d614ecca1cb
Matthew Skomarovsky <matthew.skomarovsky@gmail.com>
2023-07-27 12:09:55 -0400

    persist notes to database using api

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,45 @@
 'use client'
 
-import { useState } from 'react'
-import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import AddNoteForm from './components/AddNoteForm'
 import NoteList, { Note } from './components/NoteList'
 
-const exampleNotes: Note[] = [
-  {
-    id: 1,
-    createdAt: "2023-07-11",
-    text: "Patient presented with abdominal pain and nausea. Performed physical examination and ordered blood tests and ultrasound to assess for possible appendicitis.",
-  },
-  {
-    id: 2,
-    createdAt: "2023-07-02",
-    text: "Follow-up for diabetes management. Reviewed blood glucose logs, adjusted medication dosage, and provided dietary recommendations."
-  },
-  {
-    id: 3,
-    createdAt: "2023-06-24",
-    text: "Annual check-up. Vital signs within normal range. Conducted comprehensive physical exam, discussed age-appropriate screenings, and addressed patient's questions."
-  }
-]
-
-let nextNoteId = 4
-
 export default function Home() {
-  let [notes, setNotes] = useState<Note[]>(exampleNotes)
+  let [notes, setNotes] = useState<Note[]>([])
   let [query, setQuery] = useState<string>("")
 
-  function addNote(text: string) {
-    let date = new Date()
-    let newNote = {
-      id: nextNoteId++,
-      createdAt: date.toISOString().split("T")[0],
-      text
-    }
-    setNotes([newNote, ...notes])
+  async function fetchNotes() {
+    let response = await fetch('api')
+    let json = await response.json()
+    setNotes(json)
   }
 
-  function deleteNote(id: number) {
-    setNotes(notes.filter(note => note.id !== id))
+  useEffect(() => {
+    fetchNotes().catch(console.error)
+  }, [])
+
+  async function addNote(text: string) {
+    await fetch('/api', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text }),
+    }).catch(console.error)
+
+    fetchNotes().catch(console.error)
+  }
+
+  async function deleteNote(id: number) {
+    await fetch('/api', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id })
+    }).catch(console.error)
+
+    fetchNotes().catch(console.error)
   }
 
   return (
     <>
-      <Head>
-        <title>Solace Notes</title>
-      </Head>
       <h1 className="mb-5 text-3xl font-bold">Solace Notes</h1>
 
       <h3 className="text-2xl mb-3">New Note</h3>
